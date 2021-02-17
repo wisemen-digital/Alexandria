39d8bacc4d30aaa1796f0fe4cb9e1d6e50ad719a
David Jennes <david.jennes@gmail.com>
2021-02-17 20:47:05 +0100

    Predownload dependency for local development

--- lib/cocoapods-alexandria/env_local/pre_install.rb
+++ lib/cocoapods-alexandria/env_local/pre_install.rb
@@ -8,17 +8,40 @@ module PodAlexandria
     end
 
     def run
-      Pod::UI.puts "Cocoapods Alexandria running in local mode."
+      Pod::UI.puts 'Cocoapods Alexandria running in local mode.'
 
-      Pod::UI.title "Generating project using XcodeGen"
+      if should_predownload_dep
+        Pod::UI.title 'Pre-downloading dependency for XcodeGen'
+        predownload_dep
+      end
+
+      Pod::UI.title 'Generating project using XcodeGen'
       XcodeGen::cleanupRome
       XcodeGen::clearDependencies(options.xcodegen_dependencies_file)
       XcodeGen::generate
 
-      Pod::UI.title "Preparing environment..."
+      Pod::UI.title 'Preparing environment...'
       podfile.prepare_for_xcodegen
 
-      Pod::UI.title "Continuing with normal CocoaPods"
+      Pod::UI.title 'Continuing with normal CocoaPods'
+    end
+
+    private
+
+    def should_predownload_dep
+      File.readlines('project.yml').grep(/Pods\/AppwiseCore\/XcodeGen/).any? &&
+        !File.directory?('Pods/AppwiseCore/XcodeGen')
+    end
+
+    def predownload_dep
+      system(
+        'rm -rf Pods/AppwiseCore && '\
+        'mkdir -p Pods && '\
+        'rm -f /tmp/ac.zip && '\
+        'curl -L "https://github.com/appwise-labs/AppwiseCore/archive/master.zip" > /tmp/ac.zip && '\
+        'unzip -q -d Pods /tmp/ac.zip && '\
+        'mv Pods/AppwiseCore-master Pods/AppwiseCore'
+      )
     end
   end
 end
