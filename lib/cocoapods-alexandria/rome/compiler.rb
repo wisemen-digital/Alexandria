7dbdf02c5e70c90d319796dfc2566f610e523adb
David Jennes <david.jennes@gmail.com>
2022-08-02 20:27:26 +0200

    Fixed Rome "forgetting" to compile sub-depenencies (and deeper)

--- lib/cocoapods-alexandria/rome/compiler.rb
+++ lib/cocoapods-alexandria/rome/compiler.rb
@@ -15,15 +15,16 @@ module PodAlexandria
       deployment_target = target.platform_deployment_target
       target_label = target.cocoapods_target_label
 
-      # build each dependency of target
-      spec_names = target.specs.map { |spec| [spec.root.name, spec.root.module_name] }.uniq
-      
-      spec_names.map { |root_name, module_name|
-        next if skip_build?(root_name, module_name, sdk)
-        if File.directory?(build_path(target, module_name, sdk))
-          build_path(target, module_name, sdk)
+      # find & build all dependencies
+      project.all_dependencies_for(target).map { |target|
+        # skip if already built (product, or by other target)
+        next if skip_build?(target, sdk)
+
+        # may already be built (by another target)
+        if File.directory?(build_path(target, sdk))
+          build_path(target, sdk)
         else
-          xcodebuild(root_name, module_name, sdk, deployment_target)
+          xcodebuild(target, sdk, deployment_target)
         end
       }.compact
     end
@@ -34,38 +35,69 @@ module PodAlexandria
       { :ios => 'iphoneos', :osx => 'macosx', :tvos => 'appletvos', :watchos => 'watchos' }
     end
 
-    def skip_build?(target, module_name, sdk)
-      File.directory?(destination_path(module_name)) ||
-      File.directory?(build_path(target, module_name, sdk)) ||
-        !is_native_target?(target)
+    def skip_build?(target, sdk)
+      File.directory?(destination_path(target)) ||
+        File.directory?(build_path(target, sdk))
     end
 
-    def is_native_target?(target_name)
-      project.targets
-        .find { |t| t.name == target_name }
-        .is_a?(Xcodeproj::Project::Object::PBXNativeTarget)
-    end
-
-    def xcodebuild(target, module_name, sdk, deployment_target)
-      args = %W(-project #{sandbox.project_path.realdirpath} -scheme #{target} -configuration #{configuration} -sdk #{sdk})
+    def xcodebuild(target, sdk, deployment_target)
+      args = %W(-project #{sandbox.project_path.realdirpath} -scheme #{target.name} -configuration #{configuration} -sdk #{sdk})
       args += flags unless flags.nil? 
       
-      Pod::UI.puts "Building '#{target}' for #{sdk}..."
+      Pod::UI.puts "Building '#{target.name}' for #{sdk}..."
       Pod::Executable.execute_command 'xcodebuild', args, true
 
-      build_path(target, module_name, sdk)
+      build_path(target, sdk)
     end
 
     def project
       @project ||= Xcodeproj::Project.open(sandbox.project_path)
     end
 
-    def build_path(target, module_name, sdk)
-      "#{build_dir}/#{configuration}-#{sdk}/#{target}/#{module_name}.framework"
+    def build_path(target, sdk)
+      "#{build_dir}/#{configuration}-#{sdk}/#{target.name}/#{target.product_name}.framework"
     end
 
-    def destination_path(module_name)
-      "#{destination}/#{module_name}.framework"
+    def destination_path(target)
+      "#{destination}/#{target.product_name}.framework"
+    end
+  end
+end
+
+module Xcodeproj
+  class Project
+    module Object
+      class AbstractTarget
+        def is_native?
+          is_a?(Xcodeproj::Project::Object::PBXNativeTarget)
+        end
+      end
+
+      class PBXNativeTarget
+        def is_bundle?
+          product_type == 'com.apple.product-type.bundle'
+        end
+
+        def all_dependencies
+          dependencies
+            .filter { |d| d.target.is_native? && !d.target.is_bundle? }
+            .map { |d| [d.target] + d.target.all_dependencies }
+            .flatten.uniq
+        end
+      end
+    end
+
+    def all_dependencies_for(umbrella_target)
+      target(umbrella_target.cocoapods_target_label).all_dependencies
+    end
+
+    def target(target_name)
+      result = targets.find { |t| t.name == target_name }
+      if result.is_native?
+        return result
+      else
+        return nil
+      end
     end
   end
 end
